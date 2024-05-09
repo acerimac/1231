8d0c1b763c44d0c7971588dfc904f19c024878fb
Amanda Cerimac <168143086+acerimac@users.noreply.github.com>
2024-05-09 18:05:07 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,58 @@
+# Imprima uma mensagem de boas-vindas na tela.
+
+print('seja bem vindo(a)')
+
+# Declare uma variável booleana verdadeiro com o valor True e imprima seu tipo
+
+booleana_verdadeiro = True 
+print(type(booleana_verdadeiro))
+
+# Imprima o resultado da multiplicação de dois números decimais de sua escolha
+
+numero1 = 1.5
+numero2 = 2.5 
+print(numero1*numero2)
+
+# Imprima o resultado da divisão de dois números inteiros de sua escolha
+
+numero3 = 10
+numero4 = 2
+print(numero3/numero4)
+
+# Imprima o resultado da subtração de dois números inteiros de sua escolha
+
+numero5 = 15
+numero6 = 5
+print(numero5-numero6)
+
+# Imprima o resultado da multiplicação de dois números decimais de sua escolha
+
+numero7 = 5.5
+numero8 = 2.7
+print(numero7*numero8)
+
+# Declare uma variável numero e atribua um número inteiro. Em seguida, imprima o dobro desse número
+
+numero9 = 8
+dobro = numero9*2
+print("o dobro de", numero9, 'é', dobro) 
+
+# Crie uma calculadora de soma com as ferramentas que vc já conhece(Use apenas input, print e variavel)
+
+numero10 = float(input("Digite o primeiro número: "))
+numero11 = float(input("Digite o segundo número: "))
+soma = numero10 + numero11
+print("A soma é:", soma)
+
+# Crie um sistema de cadastro com as estruturas que vc já  conhece(Use apenas input, print e variavel)
+
+nome = input("digite seu nome: ")
+idade = input("digite sua idade: ")
+email = input("digite seu e-mail: ")
+print("Cadastro realizado com sucesso!")
+print("Nome:", nome)
+print("Idade:", idade)
+print("E-mail:", email)
+
+
+
